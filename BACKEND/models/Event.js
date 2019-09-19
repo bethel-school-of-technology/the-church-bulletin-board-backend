b3db52dfea8ec4db67e89896de2479a74cd1206a
Tamara Hoff <thoff417@hotmail.com>
2019-09-19 16:59:13 -0700

    20190919 - updates for a test file
    John Michael and Tami worked together to create a working backend connected to a server and database.

--- BACKEND/models/Event.js
+++ BACKEND/models/Event.js
@@ -1,17 +1,14 @@
-const mongoose = require('mongoose');
-
+const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
-const serviceSchema = new Schema ({
+//create schema
+const EventSchema = new Schema({
     title: {type: String, required: true},
     price: {type: String, required: true},
     description: {type: String, required: true},
     contactName: {type: String, required: true},
     contactPhone: {type: String, required: true},
-    contaceEmail: {type: String, required: true},
-},
-    {timestamps: true,}
-);
-const Service = mongoose.model('Service', serviceSchema);
+    contactEmail: {type: String, required: true},
+});
 
-module.exports = Service;
+module.exports = Event = mongoose.model('event', EventSchema);
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://guest:guest123@cluster0-gxpix.mongodb.net/test?retryWrites=true&w=majority'
+}
