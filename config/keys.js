660e9f3cd8385e8cbdbe37c80cca9e095c869747
John M Hairston <johnmhairston8@gmail.com>
2019-09-19 23:04:13 -0500

    testing pushing to dev2

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     mongoURI: 'mongodb+srv://dbuser:<password>@clusterocrush-ljprw.mongodb.net/test?retryWrites=true&w=majority'
 }
-
+//Tami's mongodb
 // module.exports = {
 //     mongoURI: 'mongodb+srv://guest:guest123@cluster0-gxpix.mongodb.net/test?retryWrites=true&w=majority'
 // }
