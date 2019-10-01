64d9db4c5aa7fdf56c094842cbf3fd9f108df122
Tamara Hoff <thoff417@hotmail.com>
2019-09-30 19:38:21 -0700

    20190930-code updates mongdb fix

--- config/keys.js
+++ config/keys.js
@@ -4,8 +4,11 @@
               //mongodb+srv://thoff:mOOkie153!@cluster0-tbv5j.mongodb.net/test?retryWrites=true&w=majority
 
 //Tami's mongodb Church Bulletin
-module.exports = {
-    mongoURI: 'mongodb+srv://thoff:mOOkie153!@cluster0-tbv5j.mongodb.net/test?retryWrites=true&w=majority'}
+//module.exports = {
+    //mongoURI: 'mongodb+srv://thoff:mOOkie153!@cluster0-tbv5j.mongodb.net/test?retryWrites=true&w=majority'}
 
+//Tami's mongodb Church Bulletin2
+module.exports = {
+    mongoURI: 'mongodb+srv://guest:guest123@cluster0-d8spu.mongodb.net/test?retryWrites=true&w=majority'}
 
 
