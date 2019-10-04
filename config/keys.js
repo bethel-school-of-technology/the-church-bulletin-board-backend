ecdb6662ab3498fa63591bf8de5b1c2b0b396603
Tamara Hoff <thoff417@hotmail.com>
2019-10-04 15:26:26 -0700

    20191004 - update keys to Church Bulletin2 where the data is

--- config/keys.js
+++ config/keys.js
@@ -8,10 +8,10 @@
     //mongoURI: 'mongodb+srv://thoff:mOOkie153!@cluster0-tbv5j.mongodb.net/test?retryWrites=true&w=majority'}
 
     //ChurchBulletin3
-module.exports = {mongoURI: 'mongodb+srv://guest:guest123@cluster0-pqexm.mongodb.net/admin?retryWrites=true&w=majority'}
+//module.exports = {mongoURI: 'mongodb+srv://guest:guest123@cluster0-pqexm.mongodb.net/admin?retryWrites=true&w=majority'}
 
-//Tami's mongodb Church Bulletin2
-//module.exports = {
-    //mongoURI: 'mongodb+srv://guest:guest123@cluster0-d8spu.mongodb.net/test?retryWrites=true&w=majority'}
+//Tami's mongodb Church Bulletin2 - all the collections of data for the classifieds, events and services reside in this connection
+module.exports = {
+    mongoURI: 'mongodb+srv://guest:guest123@cluster0-d8spu.mongodb.net/test?retryWrites=true&w=majority'}
 
 //ChurchBulletin
